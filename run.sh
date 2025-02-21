55dd6682f0b567330e6c79b48340f56a460c10ad
carengyl <pavnikol84@gmail.com>
2025-02-21 16:08:02 +0300

    Remove src

--- run.sh
+++ run.sh
@@ -4,10 +4,7 @@ QEMU=qemu-system-riscv32
 
 CC=/usr/bin/clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
-ls
-cd src
-ls
-ls ../build
+
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
 
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
