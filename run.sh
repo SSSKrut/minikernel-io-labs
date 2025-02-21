9a29e233364a29a5351d33d0de6749b048904292
Alexander Nalobin <m_troc@mail.ru>
2025-02-21 15:31:50 +0300

    Fix typo

--- run.sh
+++ run.sh
@@ -4,7 +4,9 @@ QEMU=qemu-system-riscv32
 
 CC=/usr/bin/clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
-
-$CC $CFLAGS -Wl, -Tkernel.kd -Wl, -Map=kernel.map -o kernel.elf kernel.c
+ls
+cd src
+ls
+$CC $CFLAGS -Wl, -Tkernel.ld -Wl, -Map=kernel.map -o kernel.elf kernel.c
 
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
