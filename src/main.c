583b9cd6850dcbfb882f03ea20827126f808ff84
carengyl <pavnikol84@gmail.com>
2025-02-21 15:18:29 +0300

    Add main.c and kernel.ld

--- src/main.c
+++ src/main.c
@@ -4,3 +4,13 @@ void kernel_main(void) {
     for (;;);
 }
 
+__attribute((section(".text.boot")))
+__attribute((naked))
+void boot(void) {
+    __asm__ __volatile__(
+        "mv sp, %[stack_top]\n"
+        "j kernel_main\n"
+        :
+        : [stack_top] "r" (__stack_top)
+    );
+}
